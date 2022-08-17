4fa73a3a6ce3edee10f11d10b4408c74ece28fab
ashleylorraine224 <ashleylorraine224@gmail.com>
2022-08-16 21:16:56 -0400

    Add project files.

---
+++ ATM2.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ATM2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
